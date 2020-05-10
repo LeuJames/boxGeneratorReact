0e946f6904df4cb66a635eb8a634fac74aeb40d2
James Leu <leujase@gmail.com>
2020-05-10 18:04:53 -0500

    Initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [blocks, setBlocks] = useState([]);
+  const [color, setColor] = useState('');
+  const [size, setSize] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBlock = { "color" : color, "size" : size}
+    setBlocks([...blocks, newBlock]);
+    setColor('');
+    setSize('');
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit = {handleSubmit}>
+            <label>Color:</label>
+            <input type="text" value={color} onChange={(e) => setColor(e.target.value)}/>
+            <label>Pixel size:</label>
+            <input type="text" value={size} onChange={(e) => setSize(e.target.value)}/>
+            <input type="submit" value="Create Box"/>
+        </form>
+        <button onClick={() => setBlocks([])}>Clear</button>
+        <div>
+        {
+          blocks.map((block, i) =>
+            <div
+              key = {i}
+              style= {{backgroundColor: block.color , width: block.size+'px', height: block.size+'px', margin: "10px", display: "inline-block", flexWrap: "wrap"}}
+            > 
+            </div>
+          )
+        }
+        </div>
     </div>
   );
 }
